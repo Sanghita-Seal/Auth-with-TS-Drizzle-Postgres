2ccfa00a4bd2aec525ff2394eedb9306f3669b33
Sanghita Seal <sealsanghita2005@gmail.com>
2026-04-14 10:37:27 +0530

    signup route

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,15 +1,19 @@
 import express from "express";
 import type { Express } from "express";
+import { authRouter } from "./auth/routes";
 
 export function createApllication (): Express{
     const app = express()
 
     //middlewares
+    app.use(express.json())
 
     //routes
     app.get('/', (req,res)=>{
         return res.json({message: 'Welcome to Chaicode auth service'})
     })
+
+    app.use('/auth', authRouter)
 
     return app;
 }
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -5,7 +5,7 @@ export const usersTable = pgTable('users', {
     id: uuid('id').primaryKey().defaultRandom(),
 
     firstName: varchar('first_name', {length: 45}).notNull(),
-    lastName: varchar('last_name', {length: 45}).notNull(),
+    lastName: varchar('last_name', {length: 45}),
 
     email: varchar('email', {length:322 }).notNull().unique(),
     emailVerified: boolean('email_verified').default(false),
