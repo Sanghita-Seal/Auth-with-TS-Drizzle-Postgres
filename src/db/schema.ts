7051a504c3c1fef11e3387167658317632fe7c31
Sanghita Seal <sealsanghita2005@gmail.com>
2026-04-14 09:09:57 +0530

    Drizzle setup

---
+++ src/db/schema.ts
@@ -0,0 +1,20 @@
+//Tables
+import {pgTable, uuid, varchar, text, boolean, timestamp} from 'drizzle-orm/pg-core'
+
+export const usersTable = pgTable('users', {
+    id: uuid('id').primaryKey().defaultRandom(),
+
+    firstName: varchar('first_name', {length: 45}).notNull(),
+    lastName: varchar('last_name', {length: 45}).notNull(),
+
+    email: varchar('email', {length:322 }).notNull().unique(),
+    emailVerified: boolean('email_verified').default(false),
+
+    password: varchar('password', {length: 66 }),
+    salt: text('salt'),
+
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updated_at').$onUpdate(()=> new Date()),
+
+})
+
